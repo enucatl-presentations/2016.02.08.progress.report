4d826425f7d6465e5435e72fbad5a58fa03ed7fe
Matteo Abis <gmatteo..abis@gmail.com>
2015-09-15 14:04:07 +0200

    update config for middleman-deploy

--- config.rb
+++ config.rb
@@ -35,10 +35,12 @@
 # Automatic image dimensions on image_tag helper
 # activate :automatic_image_sizes
 
-# Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
-# end
+#Reload the browser automatically whenever files change
+configure :development do
+  activate :livereload
+end
+
+set :haml, { :ugly => true }
 
 # Methods defined in the helpers block are available in templates
 # helpers do
@@ -65,8 +67,23 @@ configure :build do
   # activate :asset_hash
 
   # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
 end
+
+activate :deploy do |deploy|
+  deploy.method = :git
+  deploy.build_before = true # default: false
+  # Optional Settings
+  # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
+  # deploy.branch   = 'custom-branch' # default: gh-pages
+  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
+  # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
+end
+
+sprockets.append_path 'vendor/'
+
+ignore "vendor/reveal.js/*.md"
+ignore "vendor/reveal.js/*.html"
